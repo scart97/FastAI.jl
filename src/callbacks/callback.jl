0167de931c7543d1afb4248bc2c71c8b0db1b56c
scart97 <scart.lucas@gmail.com>
2020-08-21 00:29:11 -0300

    New callback start

---
+++ src/callbacks/callback.jl
@@ -0,0 +1,141 @@
+#= 
+callback.jl:
+
+Author: Peter Wolf (opus111@gmail.com)
+
+Port of the FastAI V2 Callback API to Julia
+
+This code is inspired by FastAI, but differs from it in important ways
+
+The original source is here
+
+https://github.com/fastai/fastai2/blob/master/fastai2/callback/core.py
+
+The documentation is copied from here
+
+https://github.com/fastai/fastai2/blob/master/docs/callback.core.html
+
+Extra influence:
+https://github.com/lorenzoh/FluxTraining.jl/blob/master/src/callbacks/callback.jl =#
+
+
+# Those go into the callback handler ?
+struct PhaseCounter
+    total_epochs::Union{Nothing,Integer}
+    epoch_idx::Union{Nothing,Integer}
+    total_batches::Union{Nothing,Integer}
+    batch_idx::Union{Nothing,Integer}
+end
+
+module Phases
+
+abstract type AbstractPhase end
+
+mutable struct TrainPhase <: AbstractPhase
+    parameters
+    counter::PhaseCounter
+end
+
+struct ValidationPhase <: AbstractPhase 
+    counter::PhaseCounter
+end
+
+struct TestPhase <: AbstractFittingPhase
+    counter::PhaseCounter
+end
+struct InitializationPhase <: AbstractPhase end
+struct CleanupPhase <: AbstractPhase end
+
+export AbstractPhase,
+    TrainPhase,
+    ValidationPhase,
+    TestPhase,
+    InitializationPhase,
+    CleanupPhase
+end
+
+using .Phases
+
+module Events
+    
+# Training events
+abstract type AbstractEvent end
+
+# BeforeFitEvent
+    # BeforeEpochEvent
+        # BeforePredictEvent - x, y
+        # BeforeLossEvent - x, y, y_hat 
+        # AfterLossEvent - x, y, y_hat, loss_val
+        # BeforeUpdateEvent - gradients
+        # CancelBatchEvent
+    # AfterEpochEvent
+    # CancelEpochEvent
+# AfterFitEvent
+# CancelFitEvent 
+
+struct BeforeFitEvent <: AbstractEvent end
+struct AfterFitEvent <: AbstractEvent end
+struct CancelFitEvent  <: AbstractEvent end
+
+struct BeforeEpochEvent <: AbstractEvent end
+struct AfterEpochEvent <: AbstractEvent end
+struct CancelEpochEvent <: AbstractEvent end
+
+mutable struct BeforePredictEvent <: AbstractEvent 
+    x
+    y
+end
+mutable struct BeforeLossEvent <: AbstractEvent 
+    x
+    y
+    y_hat 
+end
+mutable struct AfterLossEvent <: AbstractEvent 
+    x
+    y
+    y_hat
+    loss_val
+end
+mutable struct BeforeUpdateEvent <: AbstractEvent 
+    gradients
+end
+struct CancelBatchEvent <: AbstractEvent end
+
+export AbstractEvent,
+    BeforeFitEvent
+    BeforeEpochEvent
+    BeforePredictEvent,
+    BeforeLossEvent,
+    AfterLossEvent,
+    BeforeUpdateEvent,
+    CancelBatchEvent
+    AfterEpochEvent
+    CancelEpochEvent
+    AfterFitEvent
+    CancelFitEvent
+
+end
+
+using .Events
+
+struct CancelFitException <: Exception end
+struct CancelBatchException <: Exception end
+struct CancelEpochException <: Exception end
+
+
+"""
+Basic class handling tweaks of the training loop by changing a [Learner](@ref) in various events
+
+The training loop is defined in [Learner](@ref) a bit below and consists in a minimal set of instructions: looping through the data we:
+
+compute the output of the model from the input
+calculate a loss between this output and the desired target
+compute the gradients of this loss with respect to all the model parameters
+update the parameters accordingly
+
+Any tweak of this training loop is defined in a Callback to avoid over-complicating the code of the training loop, and to make it easy to mix and match different techniques (since they'll be defined in different callbacks).
+
+By default handling of these events do nothing.  Special behavior is implemented by overriding these methods
+
+"""
+abstract type AbstractCallback end
